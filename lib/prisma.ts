d3cd07148f7f6769a643f5e5338e32c0210e80e1
Odai Hatem <odaihatem@Odais-MacBook-Pro.local>
2025-10-17 08:59:03 +0400

    update

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,14 +1,14 @@
 import { PrismaClient } from "@prisma/client-student-registration";
 
 // Ensure a single PrismaClient instance across hot reloads in dev
-const globalForPrisma = globalThis as unknown as { prismaParent?: PrismaClient };
+const globalForPrisma = globalThis as unknown as { prismaStudent?: PrismaClient };
 
-export const prismaParent =
-  globalForPrisma.prismaParent ??
+export const prismaStudent =
+  globalForPrisma.prismaStudent ??
   new PrismaClient({
     log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
   });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prismaParent = prismaParent;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prismaStudent = prismaStudent;
 
-export default prismaParent;
+export default prismaStudent;
