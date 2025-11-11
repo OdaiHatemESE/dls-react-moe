51a99314e0c596e8724864c360c2fda17d454dc3
Odai Hatem <odaihatem@MacBook-Pro.local>
2025-11-11 14:55:58 +0400

    updatE

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: 'PPApp-staging',
+      name: 'PPApp-st',
       script: 'node_modules/next/dist/bin/next',
       args: 'start -p 4200',
       cwd: './',
